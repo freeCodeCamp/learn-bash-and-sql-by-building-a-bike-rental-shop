4859e4a79eb5edb5f7868d6a797fdc29e7542904
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-09 15:57:31 -0500

    210.1

--- .freeCodeCamp/test/200.test.js
+++ .freeCodeCamp/test/200.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { Client } = require('pg');
 
 const database = 'bikes';
@@ -22,4 +22,4 @@ describe('The "customers" table', () => {
       await client.end();
     }
   });
-});
+});*/
