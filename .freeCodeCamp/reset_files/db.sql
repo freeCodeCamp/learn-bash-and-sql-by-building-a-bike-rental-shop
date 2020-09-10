26674c4be5234f57e82151e9035cf2e1fd7cad7d
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-10 14:49:49 -0500

    430.1

--- .freeCodeCamp/reset_files/db.sql
+++ .freeCodeCamp/reset_files/db.sql
@@ -184,6 +184,8 @@ INSERT INTO public.bikes VALUES (1, 'Mountain', 27, true);
 INSERT INTO public.bikes VALUES (2, 'Mountain', 28, true);
 INSERT INTO public.bikes VALUES (3, 'Mountain', 29, true);
 INSERT INTO public.bikes VALUES (4, 'Road', 27, true);
+INSERT INTO public.bikes VALUES (5, 'Road', 28, true);
+INSERT INTO public.bikes VALUES (6, 'Road', 29, true);
 
 
 --
@@ -202,7 +204,7 @@ INSERT INTO public.bikes VALUES (4, 'Road', 27, true);
 -- Name: bikes_bike_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.bikes_bike_id_seq', 4, true);
+SELECT pg_catalog.setval('public.bikes_bike_id_seq', 6, true);
 
 
 --
