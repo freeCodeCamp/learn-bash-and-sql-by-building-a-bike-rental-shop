1cad2b1256cb5ce6c2bbdc555591be8315624293
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-10 14:49:49 -0500

    430.1

--- .freeCodeCamp/test/430.test.js
+++ .freeCodeCamp/test/430.test.js
@@ -1,4 +1,4 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { Client } = require('pg');
 
 const database = 'bikes';
@@ -8,20 +8,22 @@ const client = new Client({
 });
 
 describe('The "bikes" table', () => {
-  it('should have a row for 28 and 29 inch Mountain bikes', async () => {
-    const query1 = `SELECT * FROM bikes WHERE type='Mountain' AND size=28;`;
-    const query2 = `SELECT * FROM bikes WHERE type='Mountain' AND size=29;`;
+  it('should have a row for 19, 20, and 21 inch BMX bikes', async () => {
+    const query1 = `SELECT * FROM bikes WHERE type='BMX' AND size=19;`;
+    const query2 = `SELECT * FROM bikes WHERE type='BMX' AND size=20;`;
+    const query3 = `SELECT * FROM bikes WHERE type='BMX' AND size=21;`;
 
     try {
       await client.connect();
       const res1 = await client.query(query1);
       const res2 = await client.query(query2);
+      const res3 = await client.query(query3);
 
-  	  assert(res1.rows.length > 0 && res2.rows.length > 0);
+  	  assert(res1.rows.length > 0 && res2.rows.length > 0 && res3.rows.length > 0);
     } catch (err) {
       assert(false);
     } finally {
       await client.end();
     }
   });
-});*/
+});
