664aa344c164f44fb5ad094b2917adcf2959f7f3
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-09 19:25:49 -0500

    290.1

---
+++ .freeCodeCamp/test/290.test.js
@@ -0,0 +1,29 @@
+const assert = require('assert');
+const { Client } = require('pg');
+
+const database = 'bikes';
+const connectionString = `postgresql://postgres@localhost:5432/${database}`;
+const client = new Client({
+  connectionString: connectionString,
+});
+
+describe('The "rentals" table', () => {
+  it('should have a "bike_id" column as a foreign key that references the correct column', async () => {
+    const query = `SELECT tc.table_schema, tc.constraint_name, tc.table_name, kcu.column_name, ccu.table_name AS foreign_table_name, ccu.column_name AS foreign_column_name FROM information_schema.table_constraints tc JOIN information_schema.key_column_usage kcu ON tc.constraint_name = kcu.constraint_name JOIN information_schema.constraint_column_usage ccu ON ccu.constraint_name = tc.constraint_name WHERE constraint_type = 'FOREIGN KEY';`;
+
+    try {
+      await client.connect();
+      const res = await client.query(query);
+
+      const foreignKeyColumn = res.rows.findIndex(row => {
+        return row.table_name === 'rentals' && row.column_name === 'bike_id' && row.foreign_table_name === 'bikes' && row.foreign_column_name === 'bike_id';
+      });
+
+      assert(foreignKeyColumn >= 0);
+    } catch (err) {
+      assert(false);
+    } finally {
+      await client.end();
+    }
+  });
+});
