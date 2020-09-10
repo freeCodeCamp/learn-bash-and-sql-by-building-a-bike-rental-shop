27b2265626fe1ce0c7516c44897d56ff6939cd37
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-09 19:29:39 -0500

    320.1

---
+++ .freeCodeCamp/test/320.test.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const { getLastQueryResult } = require('./utils');
+
+describe('You', () => {
+  let lastQueryResult;
+  before(async () => {
+    lastQueryResult = await getLastQueryResult();
+  });
+
+  it('should display the details of the "rentals" table', () => {
+    assert(/Table"public\.rentals"/.test(lastQueryResult));
+  });
+});
