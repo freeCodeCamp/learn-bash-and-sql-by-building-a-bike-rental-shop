8de72939befd0690ed5e8bd6a286bc64cde8b767
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-14 16:34:04 -0500

    480.1

--- .freeCodeCamp/test/480.test.js
+++ .freeCodeCamp/test/480.test.js
@@ -1,13 +1,13 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getScriptOutput } = require('./utils');
 
 describe('Your script', () => {
   let scriptOutput;
   before(async () => {
-    scriptOutput = await getScriptOutput('bash ../bike-shop.sh');
+    scriptOutput = await getScriptOutput('../bike-shop.sh');
   });
 
   it('should echo the suggested output', async () => {
-    assert(/~~~~~ Bike Rental Shop ~~~~~/.test(scriptOutput));
+    assert(scriptOutput);
   });
-});*/
+});
