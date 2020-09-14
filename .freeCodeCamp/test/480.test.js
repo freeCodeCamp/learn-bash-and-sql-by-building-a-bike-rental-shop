cdea42bcbc85d982153233f6de9fe9cea26f5e86
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-14 16:35:47 -0500

    490.1

--- .freeCodeCamp/test/480.test.js
+++ .freeCodeCamp/test/480.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getScriptOutput } = require('./utils');
 
 describe('Your script', () => {
@@ -10,4 +10,4 @@ describe('Your script', () => {
   it('should echo the suggested output', async () => {
     assert(scriptOutput);
   });
-});
+});*/
