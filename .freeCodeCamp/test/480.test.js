d9f4a611d2e286c61068dcdbe024c3057f5f4087
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-14 16:34:04 -0500

    480.1

---
+++ .freeCodeCamp/test/480.test.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const { getLastCommand } = require('./utils');
+
+describe('You', () => {
+  let lastCommand;
+  before(async () => {
+    lastCommand = await getLastCommand();
+  });
+
+  it('should make the script executable with the "chmod" command', async () => {
+    assert(lastCommand[0] === 'chmod');
+  });
+});
