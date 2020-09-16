c2abd87412bca360557dbe863d9781dbf5bf8d8a
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-16 18:58:09 -0500

    650.1

---
+++ .freeCodeCamp/test/650.test.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const { getFileContents } = require('./utils');
+
+describe('You', () => {
+  let scriptFile;
+  before(async () => {
+    scriptFile = await getFileContents('../bike-shop.sh');
+  });
+
+  it('should read user input into a "MAIN_MENU_SELECTION" variable', async () => {
+    assert(/read[ \t]+MAIN_MENU_SELECTION\s+/.test(scriptFile));
+  });
+});
