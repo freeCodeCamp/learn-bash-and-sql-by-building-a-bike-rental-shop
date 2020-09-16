1c6a33898064d1632ab29b887d7ff367e62c6254
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-16 18:17:27 -0500

    560.1

---
+++ .freeCodeCamp/test/560.test.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const { getFileContents } = require('./utils');
+
+describe('Your script', () => {
+  let scriptFile;
+  before(async () => {
+    scriptFile = await getFileContents('../bike-shop.sh');
+  });
+
+  it('should clear the console at the beginning', async () => {
+    assert(/echo[ \t]+('|")~~~~~ Bike Rental Shop ~~~~~\\n\1/.test(scriptFile));
+  });
+});
