4f2e7bf78ca1d1c7fa8e33f0b0fd595e9788b1e4
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-16 17:56:52 -0500

    520.1

---
+++ .freeCodeCamp/test/520.test.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const { getFileContents } = require('./utils');
+
+describe('Your "MAIN_MENU" function', () => {
+  let scriptFile;
+  before(async () => {
+    scriptFile = await getFileContents('../bike-shop.sh');
+  });
+
+  it('should echo the correct text', async () => {
+    assert(/echo[ \t]+('|"|)How may I help you\?\1/.test(scriptFile));
+  });
+});
