8ddbff278ab0032f686a79ba67e74c74fa5c04c9
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-16 18:58:09 -0500

    650.1

--- .freeCodeCamp/bike-shop.sh
+++ .freeCodeCamp/bike-shop.sh
@@ -6,6 +6,10 @@ echo -e "~~~~~ Bike Rental Shop ~~~~~\n"
 MAIN_MENU () {
 	echo -e "How may I help you?\n"
 	echo -e "1. Rent a bike\n2. Return a bike\n3. Exit"
+	# Add your code below this line
+
+
+	# Add your code above this line
 }
 
 MAIN_MENU
