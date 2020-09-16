6c4765d10d697b03f19a9628eadca5842a40b26e
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-16 18:21:13 -0500

    590.1

--- .freeCodeCamp/test/580.test.js
+++ .freeCodeCamp/test/580.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand } = require('./utils');
 
 describe('You', () => {
@@ -10,4 +10,4 @@ describe('You', () => {
   it('should view the echo manual', async () => {
     assert(lastCommand[0] === 'man' && lastCommand[1] === 'echo');
   });
-});
+});*/
