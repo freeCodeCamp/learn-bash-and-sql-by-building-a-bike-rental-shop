22cab5873a206c6b320e9123cf6588f5296e0ae6
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-16 18:24:27 -0500

    600.1

--- .freeCodeCamp/test/590.test.js
+++ .freeCodeCamp/test/590.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getFileContents } = require('./utils');
 
 describe('You', () => {
@@ -10,4 +10,4 @@ describe('You', () => {
   it('should add the "-e" flag to your greeting', async () => {
     assert(/echo[ \t]+-e[ \t]+('|")~~~~~ Bike Rental Shop ~~~~~\\n\1/.test(scriptFile));
   });
-});
+});*/
