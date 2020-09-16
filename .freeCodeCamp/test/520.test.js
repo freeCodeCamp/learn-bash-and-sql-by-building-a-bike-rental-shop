f73b4bcd5f8332db006f5b7a25074791e28ae56b
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-16 17:56:52 -0500

    520.1

---
+++ .freeCodeCamp/test/520.test.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+const { getFileContents } = require('./utils');
+
+describe('Your "MAIN_MENU" function', () => {
+  let scriptFile;
+  before(async () => {
+    scriptFile = await getFileContents('../bike-shop.sh');
+  });
+
+  it('should echo the correct text', async () => {
+    const mainMenuFunction = scriptFile.match(/MAIN_MENU\s*\(\s*\)\s*{[\s\S]+?[^}]}/g)[0];
+
+    assert(/echo[ \t]+('|"|)How may I help you\?\1/.test(mainMenuFunction));
+  });
+});
