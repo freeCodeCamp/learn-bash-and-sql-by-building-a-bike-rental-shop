f884fa886113ed0965e2132a2b8f8f5a2d856080
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-16 19:30:13 -0500

    720.1

---
+++ .freeCodeCamp/test/720.test.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+const { getScriptOutput } = require('./utils');
+
+describe('Your script', () => {
+  let scriptOutput1, scriptOutput2, scriptOutput3;
+  before(async () => {
+    scriptOutput1 = await getScriptOutput('../bike-shop.sh', '1');
+    scriptOutput2 = await getScriptOutput('../bike-shop.sh', '2');
+    scriptOutput3 = await getScriptOutput('../bike-shop.sh', '3');
+  });
+
+  it('should take you to the various menus and ouput the correct text', async () => {
+    assert(/Rent Menu/.test(scriptOutput1) && /Return Menu/.test(scriptOutput2) && /Thank you for stopping in\./.test(scriptOutput3));
+  });
+});
