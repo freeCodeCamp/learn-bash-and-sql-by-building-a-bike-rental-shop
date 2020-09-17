075c33773b4eca5bd8bd19bb6f799bc6fc5e1ef4
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-16 19:10:00 -0500

    670.1

---
+++ .freeCodeCamp/test/670.test.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const { getFileContents } = require('./utils');
+
+describe('Your "RENT_MENU" function', () => {
+  let scriptFile;
+  before(async () => {
+    scriptFile = await getFileContents('../bike-shop.sh');
+  });
+
+  it('should echo the correct text', async () => {
+    assert(/echo[ \t]+('|"|)Rent Menu\1/.test(scriptFile));
+  });
+});
