9a95f44fe52cfafcfe1441a5433a4a9ce0df9658
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-16 19:39:37 -0500

    730.1

--- .freeCodeCamp/reset_files/bike-shop.sh
+++ .freeCodeCamp/reset_files/bike-shop.sh
@@ -6,6 +6,13 @@ MAIN_MENU() {
   echo "How may I help you?" 
   echo -e "\n1. Rent a bike\n2. Return a bike\n3. Exit"
   read MAIN_MENU_SELECTION
+
+  case $MAIN_MENU_SELECTION in
+    1) RENT_MENU ;;
+    2) RETURN_MENU ;;
+    3) EXIT ;;
+    *) MAIN_MENU ;;
+  esac
 }
 
 RENT_MENU() {
