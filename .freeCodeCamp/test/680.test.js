e2b50eedaf494bf2b0a704f7acc2cb0e11a26885
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-16 19:12:31 -0500

    690.1

--- .freeCodeCamp/test/680.test.js
+++ .freeCodeCamp/test/680.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getFileContents } = require('./utils');
 
 describe('Your script', () => {
@@ -10,4 +10,4 @@ describe('Your script', () => {
   it('should have a "RETURN_MENU" function', async () => {
     assert(/(function\s+)?RETURN_MENU[ \t]*\([ \t]*\)\s*{\s*}/.test(scriptFile));
   });
-});
+});*/
