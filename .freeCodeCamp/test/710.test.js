cec4a28f74b7de0464e7fca95454c538d7c30417
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-16 19:16:50 -0500

    710.1

---
+++ .freeCodeCamp/test/710.test.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+const { getFileContents } = require('./utils');
+
+describe('Your "EXIT" function', () => {
+  let scriptFile;
+  before(async () => {
+    scriptFile = await getFileContents('../bike-shop.sh');
+  });
+
+  it('should echo the correct text', async () => {
+    const exitFunction = scriptFile.match(/EXIT\s*\(\s*\)\s*{[\s\S]+?[^}]}/g)[0];
+
+    assert(/echo[ \t]+('|"|)Thank you for stopping in\.\1/.test(exitFunction));
+  });
+});
