4ed5e43a432baa54323ceb711c3dcee52e0855e1
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-16 19:13:53 -0500

    700.1

---
+++ .freeCodeCamp/test/700.test.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const { getFileContents } = require('./utils');
+
+describe('Your script', () => {
+  let scriptFile;
+  before(async () => {
+    scriptFile = await getFileContents('../bike-shop.sh');
+  });
+
+  it('should have an empty "EXIT_MENU" function', async () => {
+    assert(/(function\s+)?EXIT_MENU[ \t]*\([ \t]*\)\s*{\s*}/.test(scriptFile));
+  });
+});
