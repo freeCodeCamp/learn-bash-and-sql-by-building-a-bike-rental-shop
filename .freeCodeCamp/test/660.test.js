2f2ec02cd7899675d03f7a638a09b7b8192fa9e6
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-16 19:10:00 -0500

    670.1

--- .freeCodeCamp/test/660.test.js
+++ .freeCodeCamp/test/660.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getFileContents } = require('./utils');
 
 describe('Your script', () => {
@@ -10,4 +10,4 @@ describe('Your script', () => {
   it('should have a "RENT_MENU" function', async () => {
     assert(/(function\s+)?RENT_MENU[ \t]*\([ \t]*\)\s*{\s*}/.test(scriptFile));
   });
-});
+});*/
