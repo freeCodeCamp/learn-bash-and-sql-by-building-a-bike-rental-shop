eb14d95e17ff1881c9bc1f15b6fbec8c8ce3ef1a
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-30 11:44:46 -0500

    820.1

---
+++ .freeCodeCamp/test/820.test.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const { getLastLog, getLastCommand } = require('./utils');
+
+describe('You', () => {
+  let lastLog, secondToLastLog;
+  before(async () => {
+    lastLog = await getLastLog(true);
+    secondToLastLog = await getLastLog(true, 2);
+    lastCommand = await getLastCommand();
+  });
+
+  it('should run the suggested psql command from the terminal', () => {
+    const re1 = /statement:select\*frombikes;/i;
+    const re2 = /connectionauthorized:user=freecodecampdatabase=bikesapplication_name=psql/;
+
+    assert(re1.test(lastLog) && re2.test(secondToLastLog) && lastCommand.includes('-c'));
+  });
+});
