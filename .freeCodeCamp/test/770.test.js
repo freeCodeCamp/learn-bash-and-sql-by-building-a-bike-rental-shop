7e1e965e9897473ff12563c4bc74de58d61f740a
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-30 11:39:22 -0500

    770.1

---
+++ .freeCodeCamp/test/770.test.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+const { getFileContents } = require('./utils');
+
+describe('Your "EXIT" function', () => {
+  let scriptFile;
+  before(async () => {
+    scriptFile = await getFileContents('../bike-shop.sh');
+  });
+
+  it('should "sleep" for two seconds', async () => {
+    const exitFunction = scriptFile.match(/EXIT\s*\(\s*\)\s*{[\s\S]+?[^}]}/g)[0];
+
+    assert(/sleep 2[^m]s?/.test(exitFunction));
+  });
+});
