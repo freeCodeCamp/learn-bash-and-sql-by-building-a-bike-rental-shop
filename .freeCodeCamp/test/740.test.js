b2aea9e1669a1d7de678b0bbed61035056820f0f
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-29 23:41:59 -0500

    740.1

---
+++ .freeCodeCamp/test/740.test.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const { getFileContents } = require('./utils');
+
+describe('Your script', () => {
+  let scriptFile;
+  before(async () => {
+    scriptFile = await getFileContents('../bike-shop.sh');
+  });
+
+  it('should have a "SHOW_TITLE" function', async () => {
+    assert(/(function\s+)?SHOW_TITLE[ \t]*\([ \t]*\)\s*{\s*clear\s*echo[ \t]+-e[ \t]+('|"|)~~~~~ Bike Rental Shop ~~~~~\\n\2\s*}/.test(scriptFile));
+  });
+});
