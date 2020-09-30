bd5241e8ec947464beb2fe110043d98cc1a45dd2
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-30 11:45:25 -0500

    830.1

---
+++ .freeCodeCamp/test/830.test.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const { getScriptOutput } = require('./utils');
+
+describe('Your script', () => {
+  let scriptOutput;
+  before(async () => {
+    scriptOutput = await getScriptOutput('../bike-shop.sh', '1');
+  });
+
+  it('should echo the result of the correct psql command', async () => {
+    assert(/Mountain/.test(scriptOutput) && /Road/.test(scriptOutput) && /BMX/.test(scriptOutput));
+  });
+});
