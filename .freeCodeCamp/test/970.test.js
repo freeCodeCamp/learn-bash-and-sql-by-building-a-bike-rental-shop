1086e0eec8e7a994f463ce1dc361d11247295ace
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-30 13:29:37 -0500

    970.1

---
+++ .freeCodeCamp/test/970.test.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const { getFileContents } = require('./utils');
+
+describe('Your script', () => {
+  let scriptFile;
+  before(async () => {
+    scriptFile = await getFileContents('../bike-shop.sh');
+  });
+
+  it('should have the correct "AVAILABLE_BIKES" variable', async () => {
+    assert(/AVAILABLE_BIKES=\$\(\$PSQL "SELECT \* FROM bikes;?"\)/.test(scriptFile));
+  });
+});
