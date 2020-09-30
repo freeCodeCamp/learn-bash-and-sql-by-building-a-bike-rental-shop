544ca2dd7d64e59230e1cf9ad884f772ec255be7
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-30 11:45:53 -0500

    840.1

--- .freeCodeCamp/bike-shop.sh
+++ .freeCodeCamp/bike-shop.sh
@@ -24,10 +24,7 @@ MAIN_MENU () {
 RENT_MENU () {
   SHOW_TITLE
 
-	# Change code below this line
-	echo "Rent Menu"
-
-	# Change code above this line
+	echo $(psql -X --username=freecodecamp --dbname=bikes -c "SELECT * FROM bikes;")
 }
 
 RETURN_MENU () {
