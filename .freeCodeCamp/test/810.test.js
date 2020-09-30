1933dc76830735440f259144cbe7c2d1eae5b6f2
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-30 11:44:46 -0500

    820.1

--- .freeCodeCamp/test/810.test.js
+++ .freeCodeCamp/test/810.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand } = require('./utils');
 
 describe('You', () => {
@@ -10,4 +10,4 @@ describe('You', () => {
   it('should view the psql help menu', async () => {
     assert(lastCommand[0] === 'psql' && lastCommand[1] === '--help');
   });
-});
+});*/
