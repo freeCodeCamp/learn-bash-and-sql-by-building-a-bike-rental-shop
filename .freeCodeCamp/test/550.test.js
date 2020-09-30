06acb9e5bd744d0619cc373e88eddbe6e4464b95
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-29 19:06:22 -0500

    555.1

--- .freeCodeCamp/test/550.test.js
+++ .freeCodeCamp/test/550.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getFileContents } = require('./utils');
 
 describe('Your script', () => {
@@ -10,4 +10,4 @@ describe('Your script', () => {
   it('should clear the console at the beginning', async () => {
     assert(/clear/.test(scriptFile));
   });
-});
+});*/
