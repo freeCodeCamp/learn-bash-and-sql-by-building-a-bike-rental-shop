f74a3d07292d2a40206faa68a829bce695ce70cf
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-30 13:32:10 -0500

    1000.1

---
+++ .freeCodeCamp/test/1000.test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const { getLastQueryResult, getLastLog } = require('./utils');
+
+describe('You', () => {
+  let lastQueryResult, lastLog;
+  before(async () => {
+    lastQueryResult = await getLastQueryResult();
+    lastLog = await getLastLog();
+  });
+
+  it('should get the correct rows and columns with the psql prompt', async () => {
+    const test1 = !/available/.test(lastQueryResult);
+    const test2 = /bike_id/.test(lastQueryResult);
+    const test3 = /type/.test(lastQueryResult);
+    const test4 = /size/.test(lastQueryResult);
+    const test5 = /select[ \t]+.*from[ \t]+bikes[ \t]+where[ \t]+available[ \t]*=[ \t]*'(t|true)'/i.test(lastLog);
+
+    assert(test1 && test2 && test3 && test4 && test5);
+  });
+});
