f93a59d0545f1fd57c1ef3370068404f6ab4b382
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-30 13:29:37 -0500

    970.1

--- .freeCodeCamp/test/920.test.js
+++ .freeCodeCamp/test/920.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getFileContents } = require('./utils');
 
 describe('Your script', () => {
@@ -7,4 +7,4 @@ describe('Your script', () => {
 
     assert(/\/bash\s*\n+\s*PSQL="psql -X --username=freecodecamp --dbname=bikes --tuples-only -c"[ \t]*;?\s*$/m.test(scriptFile));
   });
-});
+});*/
