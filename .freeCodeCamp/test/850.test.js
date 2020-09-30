1f69dcb443eb5e353a92a17548d9927bfda7df38
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-30 11:46:29 -0500

    850.1

---
+++ .freeCodeCamp/test/850.test.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const { getFileContents, getScriptOutput } = require('./utils');
+
+describe('You', () => {
+  let scriptFile, scriptOutput;
+  before(async () => {
+    scriptFile = await getFileContents('../bike-shop.sh');
+    scriptOutput = await getScriptOutput('../bike-shop.sh', '1');
+  });
+
+  it('should add the correct flag to your psql command', async () => {
+    const rentMenuFunction = scriptFile.match(/RENT_MENU\s*\(\s*\)\s*{[\s\S]+?[^}]}/g)[0];
+    const correctFlag = /(--tuples-only|-t)/.test(rentMenuFunction);
+    const correctOutput = !/(bike_id|type|size|available|rows)/.test(scriptOutput);
+    
+    assert(correctFlag && correctOutput);
+  });
+});
