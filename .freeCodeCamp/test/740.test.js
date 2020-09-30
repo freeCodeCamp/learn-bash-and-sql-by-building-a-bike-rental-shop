bb7d589b9a844ce424032a5b530cf0fbd6cceaca
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-30 11:37:57 -0500

    750.1

--- .freeCodeCamp/test/740.test.js
+++ .freeCodeCamp/test/740.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getFileContents } = require('./utils');
 
 describe('Your "EXIT_MENU" function', () => {
@@ -10,4 +10,4 @@ describe('Your "EXIT_MENU" function', () => {
   it('should echo the correct text', async () => {
     assert(/(function\s+)?SHOW_TITLE[ \t]*\([ \t]*\)\s*{\s*clear\s*echo[ \t]+-e[ \t]+('|"|)~~~~~ Bike Rental Shop ~~~~~\\n\2\s*}/.test(scriptFile));
   });
-});
+});*/
