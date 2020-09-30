0185fa1d1aa17b50da540cddc39fc10bb5beab68
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-30 13:32:51 -0500

    1050.1

---
+++ .freeCodeCamp/test/1050.test.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+const { getFileContents } = require('./utils');
+
+describe('Your "RENT_MENU" function', () => {
+  let scriptFile;
+  before(async () => {
+    scriptFile = await getFileContents('../bike-shop.sh');
+  });
+
+  it('should have the correct if condition and statements', async () => {
+    const rentMenuFunction = scriptFile.match(/RENT_MENU\s*\(\s*\)\s*{[\s\S]+?[^}]}/g)[0]
+
+    assert(/if\s+\[\[\s+-z[ \t]*\$AVAILABLE_BIKES[ \t]+\]\](([ \t]*;\s*)|\s*(?=\n+)\s*)then\s+MAIN_MENU(([ \t]*;\s*)|\s*(?=\n+)\s*)fi/.test(rentMenuFunction));
+  });
+});
