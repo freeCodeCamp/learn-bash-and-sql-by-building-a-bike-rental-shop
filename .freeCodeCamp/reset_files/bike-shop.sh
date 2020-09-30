6118c594f030a01d990583d9b0a2a813da164002
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-30 13:48:07 -0500

    1150.1

--- .freeCodeCamp/reset_files/bike-shop.sh
+++ .freeCodeCamp/reset_files/bike-shop.sh
@@ -36,7 +36,7 @@ RENT_MENU() {
     echo -e "\nHere are the bikes we have available:"
     echo "$AVAILABLE_BIKES" | while read BIKE_ID BAR TYPE BAR SIZE
     do
-      echo "$BIKE_ID $TYPE $SIZE"
+      echo "$BIKE_ID) $SIZE\" $TYPE Bike"
     done
 
     # ask for bike to rent
