6827540c0684db75db3616cb08d63e100a719090
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-30 13:32:38 -0500

    1030.1

---
+++ .freeCodeCamp/test/1030.test.js
@@ -0,0 +1,25 @@
+const assert = require('assert');
+const { Client } = require('pg');
+
+const database = 'bikes';
+const connectionString = `postgresql://postgres@localhost:5432/${database}`;
+const client = new Client({
+  connectionString: connectionString,
+});
+
+describe('You', () => {
+  it('should set "available" to "false" for all the bikes with the psql prompt', async () => {
+    const query = "SELECT * FROM bikes WHERE available='t';"
+
+    try {
+      await client.connect();
+      const res = await client.query(query);
+
+      assert(res.rowCount === 0);
+    } catch (err) {
+      assert(false);
+    } finally {
+      await client.end();
+    }
+  });
+});
