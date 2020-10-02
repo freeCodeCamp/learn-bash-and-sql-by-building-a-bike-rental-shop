311f74f050c543cce951cd76458d71b72489edad
moT01 <20648924+moT01@users.noreply.github.com>
2020-10-02 17:32:18 -0500

    823.1

--- .freeCodeCamp/test/823.test.js
+++ .freeCodeCamp/test/823.test.js
@@ -1,4 +1,4 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getLastLog, getLastCommand } = require('./utils');
 
 describe('You', () => {
@@ -9,6 +9,6 @@ describe('You', () => {
   });
 
   it('should run your script with "./bike-shop.sh"', async () => {
-    assert(/statement:SELECT\*FROMbikes;?/i.test(lastLog) && lastCommand[0] === 'psql' && lastCommand.length > 4);
+    assert(/statement:SELECT\*FROMbikes;?/i.test(lastLog) && lastCommand[0] === 'psql' && lastCommand.length > 5);
   });
-});*/
+});
