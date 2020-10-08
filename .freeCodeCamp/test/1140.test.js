745a2a557a1de49e02859ba8deb9f100a7bba8e0
moT01 <20648924+moT01@users.noreply.github.com>
2020-10-08 12:10:22 -0500

    1142.1

--- .freeCodeCamp/test/1140.test.js
+++ .freeCodeCamp/test/1140.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getFileContents } = require('./utils');
 
 describe('Your "RENT_MENU" function', () => {
@@ -12,4 +12,4 @@ describe('Your "RENT_MENU" function', () => {
     
     assert(/echo "\$AVAILABLE_BIKES"[ \t]+\|[ \t]+while\s+read[ \t]+BIKE_ID[ \t]+BAR1[ \t]+TYPE[ \t]+BAR2[ \t]+SIZE(([ \t]*;\s*)|\s*(?=\n+)\s*)do\s+echo[ \t]+("|)\$BIKE_ID[ \t]+\$TYPE[ \t]+\$SIZE\3(([ \t]*;\s*)|\s*(?=\n+)\s*)done/.test(rentMenuFunction));
   });
-});
+});*/
