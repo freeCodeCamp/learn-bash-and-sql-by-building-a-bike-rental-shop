41182ca4882ff040b0a1a203586f92b5b11bacb3
moT01 <20648924+moT01@users.noreply.github.com>
2020-10-14 14:21:22 -0500

    1640.1

--- .freeCodeCamp/test/1630.test.js
+++ .freeCodeCamp/test/1630.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getFileContents } = require('./utils');
 
 describe('Your script', () => {
@@ -8,4 +8,4 @@ describe('Your script', () => {
 
     assert(re.test(scriptFile) && /check if input is rented\s*\n+\s*RENTAL_ID=\$\(\s*\$PSQL[\s\S]*(\$CUSTOMER_ID[\s\S]*\$BIKE_ID_TO_RETURN|\$BIKE_ID_TO_RETURN[\s\S]*\$CUSTOMER_ID)/.test(scriptFile));
   });
-});
+});*/
