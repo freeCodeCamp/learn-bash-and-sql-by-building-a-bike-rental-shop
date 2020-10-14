5b74aeab31b66f71df13e062b826a88e57e4f26b
moT01 <20648924+moT01@users.noreply.github.com>
2020-10-14 14:20:36 -0500

    1570.1

--- .freeCodeCamp/bike-shop.sh
+++ .freeCodeCamp/bike-shop.sh
@@ -84,12 +84,14 @@ RETURN_MENU () {
 
   CUSTOMER_RENTALS=$($PSQL "SELECT bikes.bike_id, type, size FROM bikes LEFT OUTER JOIN rentals ON bikes.bike_id = rentals.bike_id LEFT OUTER JOIN customers ON customers.customer_id = rentals.customer_id WHERE phone='$PHONE_NUMBER' AND date_returned IS NULL;")
 
-  # Add your code below this line
-
-
-  # Add your code above this line
+  # Change code below this line
+  if [[ -z $CUSTOMER_RENTALS ]]
+  then
+    MAIN_MENU "I could not find any rentals for that phone number."
+  fi
 
   echo $CUSTOMER_RENTALS
+  # Change code above this line
 }
 
 EXIT () {
