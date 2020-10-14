672ec415bf84c9c6ad26c16caad743e9331eeee0
moT01 <20648924+moT01@users.noreply.github.com>
2020-10-14 14:12:47 -0500

    1270.1

---
+++ .freeCodeCamp/test/1270.test.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+const { getFileContents } = require('./utils');
+
+describe('Your "RENT_MENU" function', () => {
+  let scriptFile;
+  before(async () => {
+    scriptFile = await getFileContents('../bike-shop.sh');
+  });
+
+  it('should correctly read input into a "BIKE_ID_TO_RENT" variable', async () => {
+    const rentMenuFunction = scriptFile.match(/RENT_MENU\s*\(\s*\)\s*{[\s\S]+?[^}]}/g)[0]
+    
+    assert(/if\s+\[\[\s+-z[ \t]+\$CUSTOMER_NAME[ \t]+\]\](([ \t]*;\s*)|\s*(?=\n+)\s*)then\s+echo[ \t]+"What's your name\?"(([ \t]*;\s*)|\s*(?=\n+)\s*)fi/.test(rentMenuFunction));
+  });
+});
