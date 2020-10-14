41ef0e37fa10b6e1ea672ac436ea6a331c5ce7e4
moT01 <20648924+moT01@users.noreply.github.com>
2020-10-14 14:14:23 -0500

    1330.1

--- .freeCodeCamp/reset_files/bike-shop.sh
+++ .freeCodeCamp/reset_files/bike-shop.sh
@@ -76,7 +76,8 @@ RENT_MENU() {
         fi
 
         # get customer_id
-
+        CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$PHONE_NUMBER'")
+        
         # insert bike rental
 
         # set bike availability to false
