2df6d9ba6b307e08dae02e5faf28f5fb491b0069
moT01 <20648924+moT01@users.noreply.github.com>
2020-10-14 14:11:42 -0500

    1220.1

---
+++ .freeCodeCamp/test/1220.test.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+const { getFileContents } = require('./utils');
+
+describe('Your "RENT_MENU" function', () => {
+  let scriptFile;
+  before(async () => {
+    scriptFile = await getFileContents('../bike-shop.sh');
+  });
+
+  it('should correctly read input into a "BIKE_ID_TO_RENT" variable', async () => {
+    const rentMenuFunction = scriptFile.match(/RENT_MENU\s*\(\s*\)\s*{[\s\S]+?[^}]}/g)[0]
+    
+    assert(rentMenuFunction.match(/BIKE_AVAILABILITY/g).length < 4);
+  });
+});
