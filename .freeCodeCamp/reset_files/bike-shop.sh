0971431d13274918ac20dd4d34d5fd7564870b2a
moT01 <20648924+moT01@users.noreply.github.com>
2020-10-14 14:15:26 -0500

    1380.1

--- .freeCodeCamp/reset_files/bike-shop.sh
+++ .freeCodeCamp/reset_files/bike-shop.sh
@@ -82,6 +82,7 @@ RENT_MENU() {
         INSERT_RENTAL_RESULT=$($PSQL "INSERT INTO rentals(customer_id, bike_id) VALUES($CUSTOMER_ID, $BIKE_ID_TO_RENT)")
 
         # set bike availability to false
+        SET_TO_FALSE_RESULT=$($PSQL "UPDATE bikes SET available = false WHERE bike_id = $BIKE_ID_TO_RENT")
 
         # get bike info
 
