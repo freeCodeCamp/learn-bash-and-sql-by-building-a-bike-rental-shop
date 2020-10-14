6f045b94f22eb796d47bfc62200875b82ebe7fbb
moT01 <20648924+moT01@users.noreply.github.com>
2020-10-14 14:15:26 -0500

    1380.1

--- .freeCodeCamp/bike-shop.sh
+++ .freeCodeCamp/bike-shop.sh
@@ -68,10 +68,7 @@ RENT_MENU () {
 
     CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$PHONE_NUMBER';")
     ADD_RENTAL_RESULT=$($PSQL "INSERT INTO rentals(customer_id, bike_id) values($CUSTOMER_ID, $BIKE_ID_TO_RENT);")
-    # Add your code below this line
-
-
-    # Add your code above this line
+    SET_TO_FALSE_RESULT=$($PSQL "UPDATE bikes SET available=false WHERE bike_id=$BIKE_ID_TO_RENT;")
   fi
 }
 
