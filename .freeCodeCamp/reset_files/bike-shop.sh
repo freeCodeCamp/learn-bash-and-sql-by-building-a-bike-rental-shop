4c884ee05f0c0302fbdf622ff8c6b349ce1a8488
moT01 <20648924+moT01@users.noreply.github.com>
2020-10-14 14:22:11 -0500

    1680.1

--- .freeCodeCamp/reset_files/bike-shop.sh
+++ .freeCodeCamp/reset_files/bike-shop.sh
@@ -143,7 +143,7 @@ RETURN_MENU() {
           # send to main menu
           MAIN_MENU "You do not have that bike rented."
         else
-          echo "Rental ID $RENTAL_ID found"
+
         fi
       fi
     fi
