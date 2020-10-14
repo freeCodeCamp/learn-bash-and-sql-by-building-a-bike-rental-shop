ac9164b4d83c5d79452ab23f16409a9668250eb8
moT01 <20648924+moT01@users.noreply.github.com>
2020-10-14 14:11:52 -0500

    1230.1

--- .freeCodeCamp/test/1220.test.js
+++ .freeCodeCamp/test/1220.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getFileContents } = require('./utils');
 
 describe('Your "RENT_MENU" function', () => {
@@ -12,4 +12,4 @@ describe('Your "RENT_MENU" function', () => {
     
     assert(rentMenuFunction.match(/BIKE_AVAILABILITY/g).length < 4);
   });
-});
+});*/
