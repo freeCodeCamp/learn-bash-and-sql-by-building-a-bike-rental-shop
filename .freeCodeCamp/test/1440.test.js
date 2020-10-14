37214a1c378e57353dc1de173857a79222bbb288
moT01 <20648924+moT01@users.noreply.github.com>
2020-10-14 14:18:05 -0500

    1440.1

--- .freeCodeCamp/test/1440.test.js
+++ .freeCodeCamp/test/1440.test.js
@@ -1,4 +1,4 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getFileContents } = require('./utils');
 
 describe('Your "RENT_MENU" function', () => {
@@ -10,10 +10,6 @@ describe('Your "RENT_MENU" function', () => {
   it('should correctly read input into a "BIKE_ID_TO_RENT" variable', async () => {
     const rentMenuFunction = scriptFile.match(/RENT_MENU\s*\(\s*\)\s*{[\s\S]+?[^}]}/g)[0]
 
-    const test1 = /BIKE_SIZE=\$\(\$PSQL[ \t]+"[ \t]*SELECT[ \t]+size[ \t]+FROM[ \t]+bikes[ \t]+WHERE[ \t]+bike_id[ \t]*=[ \t]*\$BIKE_ID_TO_RENT[ \t]*;?"[ \t]*\)/i.test(rentMenuFunction)
-    const test2 = /BIKE_SIZE/.test(rentMenuFunction)
-    const test3 = /\$BIKE_ID_TO_RENT/.test(rentMenuFunction);
-
-    assert(test1 && test2 && test3);
+    assert(/MAIN_MENU[ \t]+"I have put you down for the \$BIKE_SIZE\\" \$BIKE_TYPE Bike, \$CUSTOMER_NAME\."/.test(rentMenuFunction));
   });
-});*/
+});
