010da015a093a26940cc15256414e281a917f1cd
moT01 <20648924+moT01@users.noreply.github.com>
2020-10-14 14:20:06 -0500

    1550.1

---
+++ .freeCodeCamp/test/1550.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const { getLastCommand, getCwd } = require('./utils');
+
+describe('You', () => {
+  let cwd, lastCommand;
+  before(async () => {
+    lastCommand = await getLastCommand();
+    cwd = await getCwd();
+  });
+
+  it('should run your script with "./bike-shop.sh"', async () => {
+    assert(lastCommand[0] === './bike-shop.sh' && cwd === '/home/strove/project');
+  });
+});
