f0d32895ff3e6e9fcc14daabdd8ac655e75bd34e
moT01 <20648924+moT01@users.noreply.github.com>
2020-10-14 14:18:16 -0500

    1450.1

---
+++ .freeCodeCamp/test/1450.test.js
@@ -0,0 +1,25 @@
+const assert = require('assert');
+const { Client } = require('pg');
+
+const database = 'bikes';
+const connectionString = `postgresql://postgres@localhost:5432/${database}`;
+const client = new Client({
+  connectionString: connectionString,
+});
+
+describe('You', () => {
+  it('should run your script and rent the bike with "bike_id" 1', async () => {
+    const query = `SELECT * FROM rentals WHERE bike_id=2`;
+
+    try {
+      await client.connect();
+      const res = await client.query(query);
+
+      assert(res.rows.length >= 1);
+    } catch (err) {
+      assert(false);
+    } finally {
+      await client.end();
+    }
+  });
+});
