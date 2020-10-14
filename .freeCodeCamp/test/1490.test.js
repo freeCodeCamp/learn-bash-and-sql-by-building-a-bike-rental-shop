5a3e338146eaf1d157551017e298d7330bef85d0
moT01 <20648924+moT01@users.noreply.github.com>
2020-10-14 14:19:18 -0500

    1490.1

---
+++ .freeCodeCamp/test/1490.test.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const { getLastLog } = require('./utils');
+
+describe('You', () => {
+  it('should view all the data in the suggested tables', () => {
+    const lastLog = await getLastLog(true);
+    const query = /statement:select\*from(bikesinnerjoinrentals|rentalsinnerjoinbikes)using\(bike_id\);/i;
+
+    assert(query.test(lastLog) && !/ERROR/.test(lastLog));
+  });
+});
+
+
