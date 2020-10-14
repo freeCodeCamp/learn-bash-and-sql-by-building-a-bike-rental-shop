809f538e87d03e63ab10c2bc7af77ff0e348ceed
moT01 <20648924+moT01@users.noreply.github.com>
2020-10-14 14:22:27 -0500

    1710.1

--- .freeCodeCamp/reset_files/bike-shop.sh
+++ .freeCodeCamp/reset_files/bike-shop.sh
@@ -144,7 +144,8 @@ RETURN_MENU() {
           MAIN_MENU "You do not have that bike rented."
         else
           # update date_returned
-
+          RETURN_BIKE_RESULT=$($PSQL "UPDATE rentals SET date_returned = NOW() WHERE rental_id = $RENTAL_ID")
+          
           # set bike availability to true
 
           # send to main menu
