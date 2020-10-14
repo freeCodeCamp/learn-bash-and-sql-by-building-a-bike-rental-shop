84c5f852502a4cccc70e08838543a4cf3f8071f8
moT01 <20648924+moT01@users.noreply.github.com>
2020-10-14 14:13:19 -0500

    1290.1

--- .freeCodeCamp/test/1280.test.js
+++ .freeCodeCamp/test/1280.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getFileContents } = require('./utils');
 
 describe('You', () => {
@@ -7,4 +7,4 @@ describe('You', () => {
 
     assert(/\$CUSTOMER_NAME[\s\S]*then[\s\S]*What's your name\?('|")[\s\S]*read[ \t]+CUSTOMER_NAME([ \t]*;\s*|\s*\n+\s*)[\s\S]*fi\s*\n+\s*fi\s*\n+\s*fi\s*\n+\s*fi/.test(scriptFile));
   });
-});
+});*/
