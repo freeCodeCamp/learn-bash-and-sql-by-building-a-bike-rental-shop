0e8a1ad0ca5911b3413123097d4d2b1713577b65
moT01 <20648924+moT01@users.noreply.github.com>
2020-10-14 14:12:47 -0500

    1270.1

--- .freeCodeCamp/reset_files/bike-shop.sh
+++ .freeCodeCamp/reset_files/bike-shop.sh
@@ -65,11 +65,13 @@ RENT_MENU() {
         CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone = '$PHONE_NUMBER'")
 
         # if customer doesn't exist
+        if [[ -z $CUSTOMER_NAME ]]
+        then
+          # get new customer name
 
-        # get new customer name
+          # insert new customer
 
-        # insert new customer
-
+        fi
       fi
     fi
   fi
