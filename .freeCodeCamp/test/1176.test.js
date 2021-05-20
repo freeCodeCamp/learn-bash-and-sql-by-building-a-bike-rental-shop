da479913a8c4916359aa866e043de175b454caca
moT01 <20648924+moT01@users.noreply.github.com>
2021-05-20 16:47:19 -0500

    1177.1

--- .freeCodeCamp/test/1176.test.js
+++ .freeCodeCamp/test/1176.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getFileContents } = require('./utils');
 
 describe('Your script', () => {
@@ -7,4 +7,4 @@ describe('Your script', () => {
     
     assert(/BIKE_ID_TO_RENT[\s\S]*if input is not a number\s*\n+\s*if[ \t]+\[\[[ \t]+![ \t]+\$BIKE_ID_TO_RENT[ \t]+=~[ \t]+\^\[0-9\]\+\$[ \t]+\]\]([ \t]*;\s*|\s*\n+\s*)then\s*#[ \t]*send to main menu\s*\n+\s*fi[\s\S]*fi/.test(scriptFile));
   });
-});
+});*/
