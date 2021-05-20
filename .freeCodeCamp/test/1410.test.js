24bed15c153c4440a774181c036ef555ee269404
moT01 <20648924+moT01@users.noreply.github.com>
2021-05-20 16:59:48 -0500

    1411.1

--- .freeCodeCamp/test/1410.test.js
+++ .freeCodeCamp/test/1410.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand } = require('./utils');
 
 describe('You', () => {
@@ -7,4 +7,4 @@ describe('You', () => {
 
     assert(/^\s*echo\s+('|")28 \| Mountain\1\s*\|\s*sed\s+('|")s\/ \|\/"\/\2\s*;?\s*$/.test(lastCommand));
   });
-});
+});*/
