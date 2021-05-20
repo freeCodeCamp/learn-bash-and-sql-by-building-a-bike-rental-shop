5aaa6e83b45a7719bd01a79ff349c03d2e4ccb09
moT01 <20648924+moT01@users.noreply.github.com>
2021-05-20 17:22:41 -0500

    1740.1

--- .freeCodeCamp/test/1730.test.js
+++ .freeCodeCamp/test/1730.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getFileContents } = require('./utils');
 
 describe('Your script', () => {
@@ -7,4 +7,4 @@ describe('Your script', () => {
     
     assert(/SET_TO_TRUE_RESULT[\s\S]*send to main menu\s*\n+\s*MAIN_MENU[ \t]+('|")Thank you for returning your bike\.\1([ \t]*;\s*|\s*\n+\s*)fi([ \t]*;\s*|\s*\n+\s*)fi([ \t]*;\s*|\s*\n+\s*)fi([ \t]*;\s*|\s*\n+\s*)fi/.test(scriptFile));
   });
-});
+});*/
